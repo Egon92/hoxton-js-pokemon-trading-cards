f079866457b7241190fe2f324bc4f93025c97fa5
Egon <loliegon@gmail.com>
2021-11-23 14:27:48 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+ /* - Create a card using JS that represents a single pokemon, 
+        use the example image as a reference. 
+        You will also find an HTML example in the templates folder
+   - Use the exact CSS classes you see in the example HTML 
+        to obtain the same style for each card
+   - The cards should be nested inside <section class="cards"></section>
+   - Use the official-artwork object key as the images 
+        for the card. The images are all inside of the sprites key, 
+        in each pokemon object
+   - Render all the cards on the page that represents 
+        all the pokemons, recreating the same layout, using JS */
+
+
+
+    
+
+
+
