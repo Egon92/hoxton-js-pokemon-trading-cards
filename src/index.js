65948663c51d771b127ec1e4f51f079055ca688d
Egon <loliegon@gmail.com>
2021-11-23 22:09:01 +0100

    Added first pokemon

--- src/index.js
+++ src/index.js
@@ -11,8 +11,44 @@
         all the pokemons, recreating the same layout, using JS */
 
 
+     const liEl = document.body.createElement(`li`)
+     liEl.setAttribute(`class`,`card`)
+
+     const h2El = document.createElement(`h2`)
+     h2El.setAttribute(`class`,`card--title`)
+
+     const imgEl = document.createElement(`img`)
+     imgEl.setAttribute(`class`,`card--img`)
+
+     const ulEl = document.createElement(`ul`)
+     ulEl.setAttribute(`class`,`card--text`)
+
+     liEl.append(h2El, imgEl, ulEl)
+     
+     const container = document.querySelector(`.cards`)
+     container.append(liEl)
+     
+     const insideListElement1 = document.createElement(`li`)
+     const insideListElement2 = document.createElement(`li`)
+     const insideListElement3 = document.createElement(`li`)
+     const insideListElement4 = document.createElement(`li`)
+     const insideListElement5 = document.createElement(`li`)
+     const insideListElement6 = document.createElement(`li`)
+        
+     ulEl.append(insideListElement1, insideListElement2, insideListElement3, insideListElement4, insideListElement5, insideListElement6)
+        
+     h2El.textContent = `Bulbasaur`
+     imgEl.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png`
+     insideListElement1.textContent = `HP: 45`
+     insideListElement2.textContent = `ATTACK: 49`
+     insideListElement3.textContent = `DEFENSE: 49`
+     insideListElement4.textContent = `SPECIAL-ATTACK: 65`
+     insideListElement5.textContent = `SPECIAL-DEFENSE: 65`
+     insideListElement5.textContent = `SPEED: 45`
+
 
     
+
 
 
 
